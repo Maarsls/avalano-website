4913a65cc5a2ceb63132a6a4c516bc8c2367666b
Maarsls <maffey.marcel@gmx.net>
2023-03-29 12:01:04 +0200

    added seo

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -7,10 +7,9 @@ function CustomHead({ children, title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} key="desc"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="robots" content="noindex,nofollow" />
+      <meta name="robots" content="index,follow" />
       <meta name="google" content="nositelinkssearchbox" />
       <meta name="google" content="notranslate" />
-      <link rel="canonical" href="https://avalano.at" />
 
       <link rel="icon" href="/favicon.ico" />
 
